6d506f49a969ef420fc73a96a61c1027c45114d7
Caroline Konrad <ckonrad@live.unc.edu>
2022-08-17 16:21:31 -0400

    Adding admin

--- backend/models/user.js
+++ backend/models/user.js
@@ -20,6 +20,12 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
+    role: {
+      type: DataTypes.ENUM,
+      values: [
+        'reviewer', 'admin',
+      ],
+    },
     passwordDigest: DataTypes.STRING
   }, {
     sequelize,
